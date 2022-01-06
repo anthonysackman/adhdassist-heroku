6e77948ffcaf85ffb19ae83ab2820e116eef5455
Anthony Sackman <anthony.sackman1@gmail.com>
2022-01-05 23:53:29 -0500

    small db update

--- app/config.py
+++ app/config.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 class Config(object):
     load_dotenv()
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQL_DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI is None:
+        SQLALCHEMY_DATABASE_URI = os.getenv('SQL_DATABASE_URL')
     # heavy performance loss
     SQLALCHEMY_TRACK_MODIFICATIONS = False
