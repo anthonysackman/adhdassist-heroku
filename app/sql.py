98c0b3d8187b00043c14ab76f895ca4b5969638a
Anthony Sackman <anthony.sackman1@gmail.com>
2022-01-09 10:18:10 -0500

    migration script, updated config, sqlalchemy models

--- app/sql.py
+++ app/sql.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine
 from app import app
 
-engine = create_engine(app.SQLALCHEMY_DATABASE_URI)
+class SqlEngine:
+    def __init__(self) -> None:
+        self.engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
