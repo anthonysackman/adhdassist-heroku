df4281163b1c99168026cfe8ee8120db5afe506d
Anthony Sackman <anthony.sackman1@gmail.com>
2022-01-19 04:00:45 -0500

    fixed created/updated dates to use sql server defaults

--- app/models.py
+++ app/models.py
@@ -4,13 +4,17 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 # UserMixin impliments standard database configurations for user session tracking (used specifically for flask_login package)
+# includes:
+# is_authenticated, is_active, is_anonymous columns
+# includes get_id() method
 class user_account(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=True)
     #is_email_validated = db.Column(db.Boolean, nullable=False)
     email_auth_codes = db.relationship('user_email_auth', backref='user_account', lazy=True)
-
+    time_created = db.Column(db.TIMESTAMP, server_default=func.now())
+    time_updated = db.Column(db.TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
     def __repr__(self):
         return '<user_account {}>'.format(self.email)
     
@@ -28,4 +32,9 @@ class user_email_auth(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_account_id = db.Column(db.Integer, db.ForeignKey('user_account.id'))
     url_hash = db.Column(db.String(120), nullable=False)
-    created_date = db.Column(func.DateTime(timezone=True), default=func.current_time())
+    time_created = db.Column(db.TIMESTAMP, server_default=func.now())
+
+# use to pass current user to current page    
+@login.user_loader
+def load_user(id):
+    return user_account.query.get(int(id))
--- app/sql.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from app import app
-from app.base_log import _logger
-class SqlEngine:
-    def __init__(self) -> None:
-        self._log = _logger._log(__name__)
-        self.engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-        self._log.info("SqlEngine initialized")
